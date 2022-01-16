53bb5ed7aa982a979df9d11a337583a4466fab3e
makowskiignacy <makowskiignacy.it@gmail.com>
2022-01-16 14:58:46 +0100

    member

--- sql_files/users.py
+++ sql_files/users.py
@@ -1,10 +1,10 @@
 import random
 
 csv = open('random_names_fossbytes.csv', 'r')
-users = open('users.sql', 'w')
+users = open('users_without_number.sql', 'w')
 i = 1
 for line in csv:
-    users.write("INSERT INTO USER VALUES (" + str(i) + ",'" + line[:-1] + "');\n")
+    users.write("INSERT INTO MEMBER VALUES ("+"'" + line[:-1] + "');\n")
     i += 1
 
 csv.close()
