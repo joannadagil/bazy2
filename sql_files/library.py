061b5c60394089a7d5300a8b3036f95e31f96585
makowskiignacy <makowskiignacy.it@gmail.com>
2022-01-18 13:15:13 +0100

    creating dbs

--- sql_files/library.py
+++ sql_files/library.py
@@ -1,4 +1,12 @@
 import random
+import datetime
+
+start_date = datetime.date(2000, 1, 1)
+end_date = datetime.date(2022, 1, 1)
+
+time_between_dates = end_date - start_date
+days_between_dates = time_between_dates.days
+random_number_of_days = random.randrange(days_between_dates)
 
 csv = open('books.csv', 'r')
 books = open('book.sql', 'w')
@@ -6,12 +14,17 @@ authors = open('authors.sql', 'w')
 authorship = open('authorship.sql', 'w')
 bookinstance = open('bookinstance.sql', 'w')
 member = open('member.sql', 'w')
+rating = open('rating.sql', 'w')
+borrowing = open('borrowing.sql', 'w')
 
 genres = ['Action and Adventure', 'Comic Book', 'Detective and Mystery', 'Fantasy', 'Historical Fiction', 'Horror', 'Literary Fiction', 'Romance', 'Science Fiction', 'Short Stories', 'Thrillers', 'Biographies', 'Cookbooks', 'History']
 books.write("set transaction isolation level serializable;\n")
 authors.write("set transaction isolation level serializable;\n")
 authorship.write("set transaction isolation level serializable;\n")
 bookinstance.write("set transaction isolation level serializable;\n")
+member.write("set transaction isolation level serializable;\n")
+rating.write("set transaction isolation level serializable;\n")
+borrowing.write("set transaction isolation level serializable;\n")
 
 author_id_dict = {}
 author_id_index = 1
@@ -27,7 +40,12 @@ for line in csv:
     authorship.write("INSERT INTO AUTHORSHIP VALUES (" + str(author_id_dict[split_line[2]]) + "," + split_line[0] + ");\n")
     for i in range(random.randint(1, 11)):
         bookinstance.write("INSERT INTO BOOKINSTANCE VALUES (" + str(bookinstance_id_index) + "," + split_line[0] + ");\n")
+        if random.choice([True, False]):
+            borrowing.write("INSERT INTO BORROWING VALUES ( DATE '" + str(random_date) + "," + "," + str(random.randint(1, 10000)) + "," + str(bookinstance_id_index) + ");\n")
         bookinstance_id_index += 1
+    for i in range(random.randint(1, 11)):
+        random_date = start_date + datetime.timedelta(days=random_number_of_days)
+        rating.write("INSERT INTO RATING VALUES (" + str(random.randint(0, 9)) + ",DATE '" + str(random_date) + "'," + str(random.randint(1, 10000)) + "," + split_line[0] + ");\n")
 
 for key in author_id_dict:
     authors.write("INSERT INTO AUTHOR VALUES (" + str(author_id_dict[key])+ ",'" + key + "');\n")
@@ -36,9 +54,15 @@ books.write("commit;")
 authors.write("commit;")
 authorship.write("commit;")
 bookinstance.write("commit;")
+member.write("commit;")
+rating.write("commit;")
+borrowing.write("commit;")
 
 csv.close()
 books.close()
 authors.close()
 authorship.close()
 bookinstance.close()
+member.close()
+rating.close()
+borrowing.close()
