6eb0678da3ae41d6603c27aa2be40ed0c0dc0909
JoannaDagil <jd417531@students.mimuw.edu.pl>
2022-01-19 18:11:50 +0100

    proba

--- main_page.php
+++ main_page.php
@@ -74,12 +74,6 @@
       <a class="active" href="#home">Home</a>
       <a href="#about">About</a>
       <a href="#contact">Contact</a>
-      <div class="search-container">
-        <form action="katalog.php">
-          <input type="text" placeholder="Search.." name="search">
-          <button type="submit">Submit</button>
-        </form>
-      </div>
     </div>
 
   </BODY>
