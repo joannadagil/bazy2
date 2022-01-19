79c32b18676cf16ec18ff17c66256d71185061ef
JoannaDagil <jd417531@students.mimuw.edu.pl>
2022-01-19 17:07:32 +0100

    nice main page header

--- main_page.php
+++ main_page.php
@@ -1,13 +1,16 @@
 <HTML>
   <HEAD>
-    <TITLE> Biblioteka </TITLE>
+    <TITLE> Main Page </TITLE>
     <link rel="stylesheet" href="css/main.css">
   </HEAD>
   <BODY>
-    <H2> Naukowcy </H2>
-    <?PHP // Wchodzimy do PHP
-      //////////////////////////////////
-      // Tworzenie ciasteczka sesyjnego.
+
+    <div class="header">
+      <h1>Biblioteka Publiczna</h1>
+      <p>im. Ignacego Makowskiego i Joanny Dagil</p>
+    </div>
+
+    <?PHP
       session_start();
       // Zapisanie loginu i hasla w ciasteczku sesyjnym.
       $_SESSION['LOGIN'] = $_POST['LOGN'];
@@ -18,9 +21,9 @@
       // Trzeci parametr to serwer bazodanowy; na students bywa ustawiony domyslnie.
       $conn = oci_connect($_SESSION['LOGIN'],$_SESSION['PASS'],"//labora.mimuw.edu.pl/LABS");
       if (!$conn) {
-    	echo "oci_connect failed\n";
-    	$e = oci_error();
-    	echo $e['message'];
+    	  echo "oci_connect failed\n";
+    	  $e = oci_error();
+    	  echo $e['message'];
       }
       /*
       // Tworzenie wyrazenia SQL-owego. Uzycie fmurlak.naukowiec zamiast naukowiec pozwala na odczytanie tabeli inego uzytkownika.
