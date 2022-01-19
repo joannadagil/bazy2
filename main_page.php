19a53ed5beccffd2a14643f700ce3bee4a466c62
JoannaDagil <jd417531@students.mimuw.edu.pl>
2022-01-19 18:14:32 +0100

    proba

--- main_page.php
+++ main_page.php
@@ -3,7 +3,25 @@
     <TITLE> Main Page </TITLE>
     <link rel="stylesheet" href="css/main.css">
 
-    
+    <style> /* idk czemu ale jak to jest w css to nie działa*/
+        body {
+        font-family: Arial, Helvetica, sans-serif;
+        margin: 0;
+        }
+
+        /* Style the header */
+        .header {
+        padding: 80px;
+        text-align: center;
+        background: #1abc9c;
+        color: white;
+        }
+
+        /* Increase the font size of the h1 element */
+        .header h1 {
+        font-size: 40px;
+        }
+    </style>
 
   </HEAD>
   <BODY>
