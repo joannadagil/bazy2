632e950b63c11fa94f41d84cdf13541642cf695e
JoannaDagil <jd417531@students.mimuw.edu.pl>
2022-01-19 20:00:54 +0100

    menu

--- main_page.php
+++ main_page.php
@@ -31,7 +31,7 @@
 
       /* -------------------- main navi -------------------- */
       .mainnav {
-        padding: 30px 50px;
+        padding: 20px 50px;
       }
 
       .mainnav input[type=submit] {
