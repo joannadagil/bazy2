71dc53ce040132c8c03e8202bfdc9ecb98b02cbc
makowskiignacy <makowskiignacy.it@gmail.com>
2022-01-20 14:21:35 +0100

    14 21

--- wypozyczanie.php
+++ wypozyczanie.php
@@ -1,16 +1,3 @@
-<html>
-<body>
-
-<div class="header">
-  <h1>Wypożyczanie</h1>
-</div>
-
-<p><?php session_start(); echo $_POST["available"]; ?></p>
-
-</body>
-</html>
-
-
 <HTML>
   <HEAD>
     <TITLE> Katalog </TITLE>
