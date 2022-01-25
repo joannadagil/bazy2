2912641a01355ed07470e2874d66fbb2b5346fb1
makowskiignacy <makowskiignacy.it@gmail.com>
2022-01-25 12:29:07 +0100

    rank

--- rankingi.php
+++ rankingi.php
@@ -26,7 +26,7 @@
       }
 
       // Tworzenie wyrazenia SQL-owego. Uzycie fmurlak.naukowiec zamiast naukowiec pozwala na odczytanie tabeli inego uzytkownika.
-      $author_book_amount = oci_parse($conn, "WITH a AS (SELECT aname, COUNT(*) AS amount FROM author JOIN authorship ON aid = idauthor GROUP BY aname, aid ORDER BY amount DESC FETCH FIRST 100 ROWS ONLY) SELECT ROWNUM, a.* FROM a");
+      $author_book_amount = oci_parse($conn, "WITH a AS (SELECT RANK() OVER (ORDER BY COUNT(*)) rank, aname, COUNT(*) AS amount FROM author JOIN authorship ON aid = idauthor GROUP BY aname, aid ORDER BY amount DESC FETCH FIRST 100 ROWS ONLY) SELECT ROWNUM, a.* FROM a");
       // Wykonywanie wyrazenia SQL-owego
       oci_execute($author_book_amount, OCI_NO_AUTO_COMMIT);
     ?>
@@ -43,7 +43,7 @@
 
     <h2> Ranking autorów względem ilości książek posiadanych przez bibliotekę </h2>
 
-    <table> 
+    <table>
       <thead>
         <tr>
           <td> Miejsce </td>
@@ -55,7 +55,7 @@
       while (($row = oci_fetch_array($author_book_amount, OCI_BOTH))) {
         ?>
         <tr>
-          <td><?php echo $row["ROWNUM"]; ?></td>
+          <td><?php echo $row["RANK"]; ?></td>
           <td><?php echo $row["ANAME"]; ?></td>
       	  <td><?php echo $row["AMOUNT"]; ?></td>
 	    </tr>
