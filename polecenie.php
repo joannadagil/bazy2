1df32fbde9a406d0b11de7336cf4b72fc2c60848
JoannaDagil <jd417531@students.mimuw.edu.pl>
2022-01-27 22:13:46 +0100

    polecenia

--- polecenie.php
+++ polecenie.php
@@ -54,7 +54,7 @@
         echo $e['message'];
       }
 
-      $genre_oci = oci_parse($conn, "SELECT bgenre, SUM(AG_DIFF) AS WAGA FROM (SELECT a.*, (10-((SELECT BIRTH FROM MEMBER WHERE ".($_SESSION['USER']."=MID))-birth)/365) AS AG_DIFF FROM (SELECT bgenre, birth FROM BOOKINSTANCE JOIN BORROWING ON biid=idbook JOIN BOOK ON book=bid JOIN MEMBER ON idlender=mid) a) b WHERE AG_DIFF >= 0 GROUP BY bgenre ORDER BY WAGA DESC;");
+      $genre_oci = oci_parse($conn, "SELECT bgenre, SUM(AG_DIFF) AS WAGA FROM (SELECT a.*, (10-(((SELECT BIRTH FROM MEMBER WHERE ".($_SESSION['USER']."=MID)-birth)/365)) AS AG_DIFF FROM (SELECT bgenre, birth FROM BOOKINSTANCE JOIN BORROWING ON biid=idbook JOIN BOOK ON book=bid JOIN MEMBER ON idlender=mid) a) b WHERE AG_DIFF >= 0 GROUP BY bgenre ORDER BY WAGA DESC");
       oci_execute($genre_oci, OCI_NO_AUTO_COMMIT);
 
     ?>
